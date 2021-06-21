a1e8ca55a8dca79326c6e79c52fe2ae97403f09f
Adri Forczek <forczekadri@gmail.com>
2021-06-21 18:43:52 +0100

    fix: trying to get backend working

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -11,10 +11,15 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 (async () => {
   dotenv.config();
 
-  await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
-  await sequelize.sync();
-
+  try {
+    await sequelize.addModels(V0_FEED_MODELS);
+    await sequelize.addModels(V0_USER_MODELS);
+    console.log("models added");
+  
+    await sequelize.sync();
+  } catch (e){
+    console.log(e);
+  }
   console.log("Database Connected");
 
   const app = express();
